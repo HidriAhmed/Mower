be6645adda28a21daf749ed82f6d61ddb8e57a46
Ahmed Hidri <hidri.ahmd@gmail.com>
2014-03-28 12:08:34 +0100

    The mower unit test

---
+++ src/test/java/com/olbati/mower/service/test/MowingServiceTestUnit.java
@@ -0,0 +1,43 @@
+package com.olbati.mower.service.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.olbati.mower.domain.Point;
+import com.olbati.mower.domain.Position;
+import com.olbati.mower.service.MowingService;
+
+public class MowingServiceTestUnit {
+
+	public final static String CONFIG_FILE_PATH = "filePath";
+
+	MowingService mower;
+	List<Position> expectedFinalPositions = new ArrayList<Position>();
+
+	@Before
+	public void init() {
+		mower = new MowingService(CONFIG_FILE_PATH);
+		Position expectedFinalPosition1 = new Position(new Point(0, 0), "G");
+		Position expectedFinalPosition2 = new Position(new Point(0, 0), "D");
+
+		expectedFinalPositions.add(expectedFinalPosition1);
+		expectedFinalPositions.add(expectedFinalPosition2);
+	}
+
+	@Test
+	public void MowingSvcTest() {
+		// arrange
+		// act
+		List<Position> testedFinalPositions = mower.go();
+		// assert
+		assertThat(testedFinalPositions.get(0)).isEqualTo(
+				expectedFinalPositions.get(0));
+		assertThat(testedFinalPositions.get(1)).isEqualTo(
+				expectedFinalPositions.get(1));
+	}
+}
