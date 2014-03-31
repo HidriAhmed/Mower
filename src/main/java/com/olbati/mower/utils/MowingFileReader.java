804b48a2fef0bb118d4cbf2a2acb98a59b630276
Ahmed Hidri <hidri.ahmd@gmail.com>
2014-03-31 09:56:46 +0200

    Adding utils method for readng file and parsing

--- src/main/java/com/olbati/mower/utils/MowingFileReader.java
+++ src/main/java/com/olbati/mower/utils/MowingFileReader.java
@@ -6,8 +6,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.olbati.mower.domain.Ground;
+import com.olbati.mower.domain.Action;
 import com.olbati.mower.domain.Mower;
+import com.olbati.mower.domain.Orientation;
+import com.olbati.mower.domain.Point;
+import com.olbati.mower.domain.Position;
 
 public class MowingFileReader {
 
@@ -20,7 +23,7 @@ public class MowingFileReader {
 	 * @return
 	 */
 	public static List<Mower> readMowingConfigFile(String aFilePath,
-			Ground aGround) {
+			Point highestLowerPoint) {
 		List<Mower> mowers = new ArrayList<Mower>();
 
 		BufferedReader br = null;
@@ -29,10 +32,59 @@ public class MowingFileReader {
 
 			String sCurrentLine;
 
-			br = new BufferedReader(new FileReader("C:\\testing.txt"));
-			int i = 0;
+			br = new BufferedReader(new FileReader(
+					"/Users/ahidri/Documents/mowers.txt"));
+			int lineIndex = 0;
+			int mowerIndex = 0;
 			while ((sCurrentLine = br.readLine()) != null) {
+				// For the first line
+				// extract
+				if (lineIndex == 0) {
+					String[] lineKeys = sCurrentLine.split(" ");
+					int posX = Integer.parseInt(lineKeys[0]);
+					int posY = Integer.parseInt(lineKeys[1]);
+					highestLowerPoint = new Point(posX, posY);
+				} else if (lineIndex % 2 == 1) {
+					String[] lineKeys = sCurrentLine.split(" ");
 
+					int posX = Integer.parseInt(lineKeys[0]);
+					int posY = Integer.parseInt(lineKeys[1]);
+					String orientationString = lineKeys[2];
+
+					Orientation orientation = null;
+					if ("E".equals(orientationString)) {
+						orientation = Orientation.EAST;
+					} else if ("N".equals(orientationString)) {
+						orientation = Orientation.NORTH;
+					} else if ("S".equals(orientationString)) {
+						orientation = Orientation.SOUTH;
+					} else if ("W".equals(orientationString)) {
+						orientation = Orientation.WEST;
+					}
+					Point initialPoint = new Point(posX, posY);
+					Position initialPosition = new Position(initialPoint,
+							orientation);
+					Mower newMower = new Mower(initialPosition, null);
+					mowers.add(newMower);
+
+				} else if (lineIndex % 2 == 0) {
+					Mower mower = mowers.get(mowerIndex);
+					List<Action> actions = new ArrayList<Action>();
+					for (int i = 0; i < sCurrentLine.length(); i++) {
+						char currentChar = sCurrentLine.charAt(i);
+						if (currentChar == 'G') {
+							actions.add(Action.LEFT);
+						} else if (currentChar == 'D') {
+							actions.add(Action.RIGHT);
+						} else if (currentChar == 'A') {
+							actions.add(Action.FORWARD);
+						}
+
+					}
+					mower.setActions(actions);
+					mowerIndex++;
+				}
+				lineIndex++;
 			}
 
 		} catch (IOException e) {
